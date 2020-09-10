428af62fb9cb795c366833adad26a384bcdd59c4
Huzaifa Momin <47695127+huzaifamomin49@users.noreply.github.com>
2020-09-10 13:16:07 +0530

    Update bubble-sort.cpp

--- bubble-sort/bubble-sort.cpp
+++ bubble-sort/bubble-sort.cpp
@@ -13,9 +13,9 @@ int main() {
     }
     for(int i=0;i<n;i++){
         for(int j=0;j<n-i;j++){
-            if(arr[j+1]>arr[j]){
-                int tmp=arr[i];
-                arr[i]=arr[j];
+            if(arr[j]>arr[j+1]){
+                int tmp=arr[j];
+                arr[j+1]=arr[j];
                 arr[j]=tmp;
             }
         }
